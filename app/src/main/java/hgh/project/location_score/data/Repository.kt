e7ec86d46015872805091d9ca999b0a0d8f64031
hangunhee <ghee0831@naver.com>
2022-05-16 01:26:47 +0900

    kakao api

--- app/src/main/java/hgh/project/location_score/data/Repository.kt
+++ app/src/main/java/hgh/project/location_score/data/Repository.kt
@@ -1,5 +1,34 @@
 package hgh.project.location_score.data
 
+import hgh.project.location_score.BuildConfig
+import hgh.project.location_score.data.service.KakaoSearchKeywordService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
 object Repository {
 
+    private val kakaoSearchApiService: KakaoSearchKeywordService by lazy {
+        Retrofit.Builder()
+            .baseUrl(Url.KAKAO_API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(buildHttpClient())
+            .build()
+            .create()
+    }
+
+    private fun buildHttpClient():OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = if (BuildConfig.DEBUG){
+                        HttpLoggingInterceptor.Level.BODY
+                    }else{
+                        HttpLoggingInterceptor.Level.NONE
+                    }
+                }
+            )
+            .build()
 }
--- app/src/main/java/hgh/project/location_score/data/service/KakaoSearchKeywordService.kt
+++ app/src/main/java/hgh/project/location_score/data/service/KakaoSearchKeywordService.kt
@@ -8,8 +8,9 @@ import retrofit2.http.Query
 interface KakaoSearchKeywordService {
 
     @Headers("Authorization: KakaoAK ${BuildConfig.KAKAO_API_KEY}")
-    @GET("/v2/local/search/keyword.json?radius=1000")
+    @GET("v2/local/search/keyword.json?radius=1000")
     suspend fun keywordSearch(
+        @Query("query") query: String,
         @Query("x") x: String,
         @Query("y") y: String
     )
